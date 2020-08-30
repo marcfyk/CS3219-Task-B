f3e17ca5a673e7b6dcc61b66899826ad585c8251
marcfyk <marcjohnfong@gmail.com>
2020-08-30 17:13:39 +0800

    connect to db

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express'
 import cors from 'cors'
-
+import mongodb from 'mongodb'
+import { uri } from './config/mongodb-config.js'
 import { PORT, IP } from './config/config.js'
 
-const app = express()
-app.use(cors())
+const run = async () => {
+    const app = express()
+    app.use(cors())
 
-app.listen(PORT, IP, () => console.log(`listening on ${IP}:${PORT}`))
+    app.listen(PORT, IP, () => console.log(`listening on ${IP}:${PORT}`))
 
-app.use(express.json())
+    app.use(express.json())
 
+    const connection = await (new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })).connect()
+
+    app.get('/', (req, res) => {
+        if (connection) {
+            res.status(200).json({
+                message: 'API is online :)',
+            })
+        } else {
+            res.status(200).json({
+                message: 'API is offline :(',
+            })
+        }
+    })
+}
+
+run()
